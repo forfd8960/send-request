5658eccd417fe3cbdc584516ba9813f66fa6adbf
forfd8960 <forfd8960@gmail.com>
2023-07-24 09:25:49 +0800

    Refactor request

--- sendreq-v1/src/main.rs
+++ sendreq-v1/src/main.rs
@@ -9,11 +9,13 @@ struct Response {
 }
 
 #[derive(Clone)]
-struct Request {
-    method: String,
-    url: String,
-    headers: HashMap<&'static str, &'static str>,
-    body: String,
+struct Request<'a> {
+    method: &'a str,
+    url: &'a str,
+    headers: HashMap<&'a str, &'a str>,
+    body: &'a str,
+    req_data: String,
+    content: &'a [&'a str],
 }
 
 const GET: &str = "GET";
@@ -21,8 +23,110 @@ const PUT: &str = "PUT";
 const POST: &str = "POST";
 const DELETE: &str = "DELETE";
 
-impl Request {
-    fn send(&self) -> Result<(), &'static str> {
+impl<'a> Request<'a> {
+    fn new() -> Self {
+        Self {
+            method: "",
+            url: "",
+            headers: HashMap::new(),
+            body: "",
+            req_data: "".to_string(),
+            content: &[],
+        }
+    }
+    /*
+
+    the request format is:
+    method url
+
+    header1: value1
+    header2: value2
+
+    {body}
+    */
+    fn from_file(&mut self, file: String) -> Result<Self, &'a str> {
+        self.read_file_content(file);
+        let method_url = self.parse_method_and_url();
+        match method_url {
+            Err(e) => return Err(e),
+            Ok(v) => {
+                self.method = &v.0.clone();
+                self.url = &v.1.clone();
+            }
+        }
+
+        let headers = self.parse_headers();
+        match headers {
+            Err(e) => return Err(e),
+            Ok(v) => {
+                self.headers = v;
+            }
+        }
+
+        self.parse_body();
+        Ok(self)
+    }
+
+    fn read_file_content(&mut self, file: String) {
+        self.req_data = fs::read_to_string(file).expect("not able to read the file");
+        let parts = self.req_data.split("\n");
+        let content: Vec<&str> = parts.collect();
+        if content.len() < 5 {
+            println!("invalid request data: {:?}", content);
+            process::exit(1);
+        }
+        self.content = &content[..];
+    }
+
+    fn parse_method_and_url(&self) -> Result<(String, String), &'a str> {
+        let method_url = self.content.get(0);
+
+        match method_url {
+            None => {
+                return Err("invalid request data");
+            }
+            Some(v) => {
+                let tmp: Vec<&str> = v.split(" ").collect();
+                if tmp.len() != 2 {
+                    return Err("invalid method and url");
+                }
+
+                let method = tmp.get(0).unwrap();
+                let url = tmp.get(1).unwrap();
+                Ok((method.trim().to_string(), url.trim().to_string()))
+            }
+        }
+    }
+
+    fn parse_headers(&self) -> Result<HashMap<&'a str, &'a str>, &'a str> {
+        let mut res: HashMap<&'a str, &'a str> = HashMap::new();
+        for kv in self.content {
+            if kv.len() == 0 {
+                break;
+            }
+
+            let kv_tmp: Vec<&str> = kv.split(":").collect();
+            if kv_tmp.len() != 2 {
+                return Err("invlid header");
+            }
+
+            res.insert(kv_tmp.get(0).unwrap().trim(), kv_tmp.get(1).unwrap().trim());
+        }
+
+        Ok(res)
+    }
+
+    fn parse_body(&self) {
+        if 2 + self.headers.len() >= self.content.len() {
+            self.body = "";
+            return;
+        }
+
+        let body_content = &self.content[2 + self.headers.len()..];
+        self.body = body_content.join("").trim();
+    }
+
+    fn send(&self) -> Result<(), &'a str> {
         let client = Client::new();
 
         let mut hdrs = reqwest::header::HeaderMap::new();
@@ -38,7 +142,7 @@ impl Request {
             println!("resp: {:?}", resp);
         };
 
-        match self.method.as_str() {
+        match self.method {
             GET => {
                 let builder = client.get(url);
                 send_with_body(builder);
@@ -84,115 +188,16 @@ fn main() {
     }
 
     let file = args.get(0).unwrap();
-
-    let request_result = parse_request_file(&file);
-    match request_result {
-        Ok(req) => {
-            let resp = req.send();
-            println!("send request result: {:?}", resp);
-        }
+    let mut request = Request::new();
+    let req = request.from_file(*file);
+    match req {
         Err(e) => {
-            println!("parse request file error: {}", e);
+            println!("{}", e);
             process::exit(1);
         }
-    }
-
-    println!("Hello, world!");
-}
-
-/*
-
-the request format is:
-method url
-
-header1: value1
-header2: value2
-
-{body}
-*/
-fn parse_request_file(req_file: &str) -> Result<Request, &'static str> {
-    let req_data = fs::read_to_string(req_file).expect("not able to read the file");
-    let parts = req_data.split("\n");
-
-    let content: Vec<&str> = parts.collect();
-    if content.len() < 5 {
-        return Err("invalid request data");
-    }
-
-    let method_url = parse_method_url(&content);
-    let mut method: String = "".to_string();
-    let mut url: String = "".to_string();
-    match method_url {
-        Err(e) => return Err(e),
         Ok(v) => {
-            method = v.0.trim().to_string();
-            url = v.1.trim().to_string();
+            let resp = req.unwrap().send();
+            println!("send request result: {:?}", resp);
         }
     }
-
-    let headers = parse_headers(&content[2..]);
-    if headers.is_err() {
-        return Err(headers.err().unwrap());
-    }
-
-    let hrds = headers.unwrap();
-    let mut body = "".to_string();
-    if 2 + hrds.len() >= content.len() {
-        body = "".to_string();
-    } else {
-        body = parse_body(&content[2 + hrds.len()..]);
-    }
-
-    Ok(Request {
-        method: method,
-        url: url,
-        headers: hrds,
-        body: body.to_string(),
-    })
-}
-
-fn parse_method_url(content: &Vec<&str>) -> Result<(String, String), &'static str> {
-    let method_url = content.get(0);
-
-    match method_url {
-        None => {
-            return Err("invalid request data");
-        }
-        Some(v) => {
-            let tmp: Vec<&str> = v.split(" ").collect();
-            if tmp.len() != 2 {
-                return Err("invalid method and url");
-            }
-
-            let method = tmp.get(0).unwrap();
-            let url = tmp.get(1).unwrap();
-            Ok((method.trim().to_string(), url.trim().to_string()))
-        }
-    }
-}
-
-fn parse_headers<'a>(
-    content: &[&'a str],
-) -> Result<HashMap<&'static str, &'static str>, &'static str> {
-    let mut res: HashMap<&'static str, &'static str> = HashMap::new();
-    for kv in content {
-        if kv.len() == 0 {
-            break;
-        }
-
-        let kv_tmp: Vec<&str> = kv.split(":").collect();
-        if kv_tmp.len() != 2 {
-            return Err("invlid header");
-        }
-
-        res.insert(kv_tmp.get(0).unwrap().trim(), kv_tmp.get(1).unwrap().trim());
-    }
-
-    Ok(res)
-}
-
-fn parse_body(content: &[&str]) -> String {
-    let body_content = content.join("\n");
-    let body = body_content.trim();
-    body.to_string()
 }
