ac5c7bdd4f09334706683cbc8ab07e5acdd3a6e6
forfd8960 <forfd8960@gmail.com>
2023-10-30 09:49:50 +0800

    [sendreqv2] User Entry - part0

--- sendreqv2/src/requestsender/mod.rs
+++ sendreqv2/src/requestsender/mod.rs
@@ -1,9 +1,25 @@
 use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
-pub struct Sender {
+pub struct Request {
     method: String,
     url: String,
     headers: HashMap<String, String>,
     body: String,
+}
+
+impl Request {
+    pub fn new(
+        method: String,
+        url: String,
+        headers: HashMap<String, String>,
+        body: String,
+    ) -> Self {
+        Self {
+            method: method,
+            url: url,
+            headers: headers,
+            body: body,
+        }
+    }
 }
--- sendreqv2/src/userentry/mod.rs
+++ sendreqv2/src/userentry/mod.rs
@@ -1,7 +1,53 @@
-pub struct Entry {}
+use crate::requestsender::Request;
+use egui::{self, Ui};
 
-impl Entry {
-    pub fn new() -> Self {
-        Self {}
+pub struct Entry<'a> {
+    left: &'a LeftSide<'a>,
+}
+
+pub struct LeftSide<'a> {
+    topbar: &'a TopBar<'a>,
+    collections: &'a Collections<'a>,
+}
+
+pub struct TopBar<'a> {
+    ui: &'a mut Ui,
+}
+
+pub struct Collections<'a> {
+    ui: &'a mut Ui,
+    collections: Vec<Collection>,
+}
+
+pub struct Collection {
+    id: i64,
+    name: String,
+    requests: Vec<Request>,
+}
+
+impl<'a> TopBar<'a> {
+    pub fn new(ui: &'a mut Ui) -> Self {
+        Self { ui: ui }
+    }
+
+    pub fn add_new_button(&mut self) {
+        if self.ui.button("New").clicked() {
+            // show a new tab in the right side
+            println!("should show a new tab in the right side");
+        }
+    }
+    pub fn add_import_button(&mut self) {
+        if self.ui.button("Import").clicked() {
+            // show import dialog
+            println!("should show a import dialog");
+        }
     }
 }
+
+// impl<'a> Entry<'a> {
+//     pub fn new(ui: &'a mut Ui) -> Self {
+//         Self {
+//             ui:
+//         }
+//     }
+// }
